ff2bfee65b64901c184400e8c86187ab54f17ae8
CosmeNicolas <nicomas7590@gmail.com>
2023-12-29 17:20:43 -0300

    agregue el componente footer al componente padre app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import "bootstrap/dist/css/bootstrap.min.css"
 import './App.css'
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
 
 function App() {
  
 
   return (
     <>
+    <Container className="main">
+      <h1>Algo va aqui</h1>
+    </Container>
+    <Footer/>
      
     </>
   )
